bda0ef3f45dc95accad6b66304d10d0f00e17d1e
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-02-15 22:45:06 +0300

    1.6

--- Main.java
+++ Main.java
@@ -4,28 +4,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person tom = new Person();
-        Person bob = new Person();
-
-        tom.displayId();    // Id = 105
-        bob.displayId();    // Id = 106
+        Person tom = new Person("Tom");
+        tom.display();
+        Person sam = new Employee("Sam", "Oracle");
+        sam.display();
     }
 }
-class Person{
+class Person {
 
-    private int id;
-    static int counter;
+    private String name;
 
-    static{
-        counter = 105;
-        System.out.println("Static initializer");
+    public String getName() { return name; }
+
+    public Person(String name){
+
+        this.name=name;
     }
-    Person(){
-        id=counter++;
-        System.out.println("Constructor");
-    }
-    public void displayId(){
 
-        System.out.printf("Id: %d \n", id);
+    public void display(){
+
+        System.out.printf("Person %s \n", name);
+    }
+}
+
+class Employee extends Person{
+
+    private String company;
+
+    public Employee(String name, String company) {
+
+        super(name);
+        this.company = company;
+    }
+    @Override
+    public void display(){
+
+        System.out.printf("Employee %s works in %s \n", super.getName(), company);
     }
 }
